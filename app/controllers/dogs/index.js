93511362329ee1dc24df9e38057572f65242ef1a
Joe Gesualdo <joegesualdo@gmail.com>
2014-06-07 14:48:43 -0400

    Build a form to create a new dog

--- app/controllers/dogs/index.js
+++ app/controllers/dogs/index.js
@@ -1,2 +1,20 @@
 export default Ember.ArrayController.extend({
+  actions: {
+    createDog: function(){
+      // Create a new dog
+      var dog = this.store.createRecord('dog', {
+            name: this.get('dogName'),
+            age: this.get('dogAge'),
+            photo: this.get('dogPhoto')
+          });
+
+      // Persist the new dog
+      dog.save();
+
+      // Clear the form
+      this.set('dogName', '');
+      this.set('dogAge', '');
+      this.set('dogPhoto', '');
+    }
+  }
 });
